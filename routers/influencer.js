180e65757f610bfa4629ec609059a2ebe599b070
diarih <diarihza@gmail.com>
2021-12-19 15:54:28 +0700

    influencer setup

---
+++ routers/influencer.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const InfluencerController = require("../controllers/influencer.controller")
+
+router.get("/", InfluencerController.getInfluencer)
+
+router.get("/:id", InfluencerController.getInfluencerByID)
+
+module.exports = router
