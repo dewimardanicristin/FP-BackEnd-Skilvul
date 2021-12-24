69847f6d31a964f573664fc81f65ddf8c37f657b
dewimardanic <dewimardanic@gmail.com>
2021-12-24 15:14:33 +0800

    fixing routes

--- routers/index.js
+++ routers/index.js
@@ -3,10 +3,14 @@ const router = express.Router()
 
 const insightDataRoutes = require('./insightData')
 const historyRoutes = require('./history')
-const admin = require('./influencer')
-const searchInfluence = require('./admin')
+const searchInfluence = require('./influencer')
+const admin = require('./admin')
 const user = require('./user')
 const bidangUsaha = require('./bidangUsaha')
+const {addOne} = require('../controllers/user')
+const {createOneHistory} = require('../controllers/history.controller')
+const LoginController = require('../controllers/login.controller')
+const AuthMiddleware = require('../middlewares/authenticate')
 // this routes just for testing guys
 router.get("/ping",(req,res) => {
     const ready = {
@@ -18,7 +22,10 @@ router.use("/insightData",insightDataRoutes)
 router.use("/history",historyRoutes)
 router.use("/admin",admin)
 router.use("/search-influence", searchInfluence)
-router.use("/user",user)
 router.use("/bidangUsaha", bidangUsaha)
+router.use("/profile",AuthMiddleware,user)
+router.post("/formPengajuan/:id",createOneHistory)
+router.post("/register", addOne)
+router.post("/login",LoginController.loginUser)
 
 module.exports = router
